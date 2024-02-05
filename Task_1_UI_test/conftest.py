ce95e92f51f8e8d38ab30910a36d0f9a7a5b7a41
YevheniiSeleznov <149239422+YevheniiSeleznov@users.noreply.github.com>
2024-02-05 15:01:34 +0200

    Delete Task_1_UI_test directory

--- Task_1_UI_test/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.get("https://www.epam.com")
-    yield driver
-    driver.quit()
